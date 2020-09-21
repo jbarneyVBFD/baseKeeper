7a90a72b3af8e165cfdb03ebad0f3b986d79ea80
John Barney <jfbarney1986@yahoo.com>
2020-09-20 21:06:31 -0400

    add lineupVC and customize picker

--- baseKeeper/Controller/LineupVC.swift
+++ baseKeeper/Controller/LineupVC.swift
@@ -8,15 +8,45 @@
 
 import UIKit
 
-class LineupVC: UIViewController {
+class LineupVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return fopr the row and component (column) that's being passed in
+    internal func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
 
+    
+    @IBOutlet weak var positionPicker: UIPickerView!
+    
+    //var pickerData: [Player.position] = [Player.position]()
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Connect data:
+        self.positionPicker.delegate = self
+        self.positionPicker.dataSource = self
+        // Input the data into the array
+        //pickerData = [Player.position.P, Player.position.C, Player.position.FIRST, Player.position.THIRD, Player.position.SECOND, Player.position.SS, Player.position.LF, Player.position.CF, Player.position.DH, Player.position.bench]
+        
+        pickerData = ["P", "C", "1B", "3B", "2B", "SS", "LF", "CF", "RF", "DH", "bench"]
     }
     
-
+    
+    
+    
     /*
     // MARK: - Navigation
 
